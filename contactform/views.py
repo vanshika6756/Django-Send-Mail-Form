fde2b27f9f0c25d156eb6ea02cad88db34d5c39e
vanshika6756 <vanshikasinghal702@gmail.com>
2022-11-18 13:33:15 +0530

    Modeifications Done

--- contactform/views.py
+++ contactform/views.py
@@ -11,9 +11,14 @@ def index(request):
         message = request.POST.get('message')
 
         email_from = settings.EMAIL_HOST_USER
+        org_mail = ['vanshika2115471@akgec.ac.in',]
+
+        subject_user = "Concern Recieved Sucessfully"
+        message_user = f"Hello {name}, Your concern has been recieved."
         recipient_list = [email]
 
-        send_mail(subject, message, email_from, recipient_list)
+        send_mail(subject, message, email_from, org_mail)
+        send_mail(subject_user, message_user, email_from, recipient_list)
         # third param is the email address we send mail from , leaveblank (optional) ''
         return HttpResponse('Thank you for subnmitting the from, check email for confirmation')
 
