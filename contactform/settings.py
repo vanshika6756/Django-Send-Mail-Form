f3f8444948256435e41f31ae6e50885dd59aea45
vanshika6756 <104566215+vanshika6756@users.noreply.github.com>
2022-11-24 12:04:31 +0530

    Update settings.py

--- contactform/settings.py
+++ contactform/settings.py
@@ -126,8 +126,8 @@ STATIC_URL = '/static/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'vanshikasinghal702@gmail.com'
-EMAIL_HOST_PASSWORD = 'utdpbdevgxpzmruw'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
